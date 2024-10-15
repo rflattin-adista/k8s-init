2f715900b86c62fa85e6ca7312625bf31b2e45c5
rflattin-adista <85351876+rflattin-adista@users.noreply.github.com>
2024-10-15 10:34:59 +0200

    Update and rename adista-monitor.sh to monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
-mkdir /var/log/adista-monitor
-printf "Memory\tDisk\tCPU\n" > /var/log/adista-monitor/general.log
+mkdir /var/log/monitor
+printf "Memory\tDisk\tCPU\n" > /var/log/monitor/general.log
 while [ true ]
 do
 	DATE=$(date '+%Y-%m-%d')
 	HOUR=$(date '+%H%M%S')
 	TS="$DATE $HOUR	"
-	if [ ! -d /var/log/adista-monitor/$DATE ] ; then
-		mkdir /var/log/adista-monitor/$DATE
+	if [ ! -d /var/log/monitor/$DATE ] ; then
+		mkdir /var/log/monitor/$DATE
 	fi
 	MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t", $3*100/$2 }')
 	DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
-	top -bn1 -c > /var/log/adista-monitor/$DATE/$HOUR
+	top -bn1 -c > /var/log/monitor/$DATE/$HOUR
 	CPU=$(cat /var/log/adista-monitor/$DATE/$HOUR | grep load | awk '{printf "%.2f%%\t\t\n", $(NF-2)}')
-	echo "$TS$MEMORY$DISK$CPU" >> /var/log/adista-monitor/general.log
+	echo "$TS$MEMORY$DISK$CPU" >> /var/log/monitor/general.log
 	# Suppress sub-directory older than 30 days
-	find /var/log/adista-monitor -type d -ctime +30 -exec /bin/rm -rf {} \;
+	find /var/log/monitor -type d -ctime +30 -exec /bin/rm -rf {} \;
 	sleep 120
 done
